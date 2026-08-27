a90f400ab6cf70dcbdab2499dec3c3d3743c2ec8
Alfredo A <almenaresalfredo@gmail.com>
2026-08-26 22:02:15 -0400

    Up to 19.1

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+
+    map.iter().filter(|e| *e.1==value ).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,7 +50,8 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
-}
+    collection.iter().fold(0, |acc, x| acc + count_iterator(x, value))
+} 
 
 fn main() {
     // You can optionally experiment here.
